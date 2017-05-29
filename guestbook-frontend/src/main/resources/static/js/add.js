0f1762de4b356a24252686a6ea2168053be1f1df
mouradNajdDevoteam <mourad.najd@devoteam.com>
2017-05-29 17:20:52 +0200

    ff

---
+++ guestbook-frontend/src/main/resources/static/js/add.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+	$(".successarea").hide();
+	$("#send").on("click",function(){
+		var data={
+				title:$("#title").val(),
+		        comment:$("#comment").val(),
+		       commenter:$("#commenter").val()
+		};
+
+		$.ajax({
+			url:"http://localhost:8080/guestbook/",
+			method:"PUT",
+			contentType:"application/json",
+			data:JSON.stringify(data),
+			success:function(data){
+			$(".successarea").show();
+			$("form").hide();
+			},
+			error:function(data){
+				alert("Etwas hat nicht geklappt!");
+			}
+		});
+	});
+})
